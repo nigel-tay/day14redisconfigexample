4a9b55be9647f3e987a1aba627a2d6691003658b
Nigel Tay <82434359+nigel-tay@users.noreply.github.com>
2023-05-18 15:48:11 +0800

    first commit

---
+++ src/main/java/sg/nus/iss/vttp/day14workshop14/Day14workshop14Application.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.vttp.day14workshop14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day14workshop14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day14workshop14Application.class, args);
+	}
+
+}
